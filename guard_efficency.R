c7ece0eb8b4ca26e7b688960d33f581923a73303
benberinsky <145381927+benberinsky@users.noreply.github.com>
2024-10-28 11:43:01 -0400

    Update guard_efficency.R

--- guard_efficency.R
+++ guard_efficency.R
@@ -1,17 +1,19 @@
-guard_efficiency <- function(
-    ppg, apg, to_pg, fg_pct, threes_taken, three_pt_pct, ft_pct, ft_taken){
-  
-  efficiency = (ppg * fg_pct) + 
-    2 * (apg / to_pg) +         
-    (ft_taken * ft_pct) +     
-    3 * (threes_taken * three_pt_pct)  
-  
-  return(efficiency)
+guard_efficiency <- function(ppg, apg, to_pg, fg_pct, threes_taken,
+                             three_pt_pct, ft_pct, ft_taken, stl, rebs, fouls, mins) {
+  efficiency = 1.5 * (ppg * fg_pct) + 
+    6 * (apg / to_pg) +         
+    1.5 * (ft_taken * ft_pct) +     
+    2.5 * (threes_taken * three_pt_pct) + 
+    3 * (stl) + 
+    3 * (rebs) - 
+    3 * (fouls)
+  return(efficiency / mins)
 }
-
-luka_efficency = guard_efficiency(33.9, 9.8, 4, 0.487, 10.6, 0.382, 0.786, 8.7)
-maxey_efficency = guard_efficiency(25.9, 6.2, 1.7, .45, 8.1, 0.373, 0.868, 5.4)
-dwhite_efficency = guard_efficiency(15.2, 5.2, 1.5, .461, 6.8, .396, 2.1, .901)
-drob_efficency = guard_efficiency(12.9, 2.8, 1.4, 0.45, 7, .395, 1.3,	.889)
-
-plot()
+guard_efficency_grade <- function(efficency){
+  if(efficency > 2){
+    return(100)
+  }
+  else{
+    return(efficency * 50)
+  }
+}
