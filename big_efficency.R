28b08943fd266b970afc130212b4cc25fd683ace
paulzhao11 <bcc3yx@virginia.edu>
2024-11-11 11:46:05 -0500

    Update big_efficency.R

--- big_efficency.R
+++ big_efficency.R
@@ -7,10 +7,17 @@ naz_efficiency = big_efficiency(13.5, 0.477, 0.9, 1.4)
 bam_efficency = big_efficiency(19.3, 0.521, 2.2, 2.3)
 
 
-big_defense <- function(steals, def_rebounds, blocks, foul, minutes){
-  defense_eff <- (0.5*steals + 0.2*def_rebounds + 0.7*blocks - 0.15*foul) / minutes
+big_efficiency <- function(points, assists, fg_pct, steals, def_rebounds, off_rebounds, blocks, foul, minutes){
+  big <- (2*(points *fg_pct) + 2*assists + 3*steals + 5*def_rebounds + 4*off_rebounds + 4*blocks - 2*foul)
+  big/minutes
 }
 
-jjj_efficiency = big_defense(1.2, 4.2, 1.6, 3.6, 32.2)
-giannis_efficiency = big_defense(1.2, 8.8, 1.1, 2.9, 35.2)
-kat_efficiency = big_defense(0.7, 6.8, 0.7, 3.3, 32.7)
+
+
+jokic_efficiency = big_efficiency(26.4, 9.0, 0.583, 1.4, 9.5, 2.8, 0.9, 2.5, 34.6)
+rudy_efficiency = big_efficiency(14.0, 1.3, 0.661, 0.7, 9.2, 3.8, 2.1, 3.1, 34.1)
+jonas_efficiency = big_efficiency(12.2, 2.1, 0.559, 0.4, 6.5, 2.3, 0.7, 0.8, 23.5)
+
+uva_1_blake = big_efficiency(10, 5, 0.5, 0, 6, 1, 3, 2, 29)
+uva_1_tj = big_efficiency(3, 1, 0.333, 1, 3, 0, 0, 1, 19)
+uva_1_jc = big_efficiency(16, 1, (7/8), 1, 5, 1, 1, 1, 29)
